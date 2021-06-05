ee734de4c3ce1ac1f4da92fc6c9e86d80aef5679
Arwa <marina-arwa@hotmail.com>
2021-06-05 16:21:50 +0200

    Syntax error handling

---
+++ input2.c
@@ -0,0 +1,128 @@
+// This is a comment
+// !====
+
+const string zed ="hhhh"; // new comment
+abc( a + x(1) * y(2));
+switch(a){
+    case 1: 
+        a++;
+        b=1+2;
+        break;
+    case 2:
+        x = 1+3;
+        if(x){
+            x++;
+            y = 1;
+        }
+        else{
+            if(a == 3){
+                d = 2;
+                
+
+            }
+        }
+}
+
+
+while(x==1){
+    y++;
+    if(x && 1){
+        a = a * 2;
+    }
+}
+
+for(;;){
+}
+
+for(int i = 0; i<2; i++){
+    func(x);
+}
+
+for(;i<2;){
+    x = 2;
+}
+
+
+do{
+    i++;
+}
+while(i<10);
+
+
+void fun(int a,int b){
+    int a,c=5+3;
+    return c;
+}
+int a=4,b;
+int a,b;
+int a,b=4;
+int a=4,b=4;
+const int a=5,b=6;
+int main(){
+int a,d=8+5;
+variable=7;
+a = a + 3+5+6+8;
+b = 5+3;
+int a,s=0;
+const string zed ="hhhh";
+const int f =6 ;
+int a,c=5+3;
+a++;
+a--;
+// ffff == a*2;
+// ffff*2 > 5 ;
+// gh+5 == true;
+// gh>4;
+bool y;
+bool g = b && d;
+const bool g = b||d;
+const bool ggg = b|d;
+if(y==true && y>5 || gh>4 && true){
+    int a=3+b&5;
+}
+a=! x(2);
+//! x(1); // This is invalid c++
+if(! x(2)){
+    int a=1;
+}
+bool c=!d;
+bool g = !(b&&d);
+bool f=!(b!=d);
+bool f=!(b>d);
+int a=3*b|5;
+b=a*h^6;
+c=a|z;
+c=a&y|v;
+c=a&y&f;
+int a=3+b*5;
+int a=3*b+5;
+
+if (x%2==0 && c==true ){
+    x=1;
+}
+else{
+    x=2;
+}
+
+
+if(x(2)!=5){
+    int d= a+((b/4^5)+7);    
+}
+}
+bool isPrimefunc(int n){
+    bool isPrime = true;
+
+
+    // 0 and 1 are not prime numbers
+    if (n == 0 || n == 1) {
+        isPrime = false;
+    }
+    else {
+        for (int i = 2; i <= n / 2; ++i) {
+            if (n % i == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+    }
+}
